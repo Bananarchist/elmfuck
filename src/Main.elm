0319c0fe5dd49e1815e31f60e781cc579f3fd23c
Zach Holker <drsturm@gmail.com>
2020-11-11 14:34:41 -0800

    Flesh out op processing
    Changed some of the op types to be more explicit because Just (Jump
    Place) is too fucking verbose
    
    This revealed some questions I need to figure out:
    - How to handle stdin buffer when empty (flags to await input?)
    - Best way to handle [] (a stack prolly, but how best to identify
      jump-to point?)
    - Is it best to work purely with ints and transform to/from chars
      solely on input/output?

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (BFCommand(..), BFDirection(..), BFTarget(..), decodeChar)
+module Main exposing (..)
 
 import Array exposing (..)
 
@@ -8,41 +8,39 @@ type alias BFModel =
     , position : Int
     , pointer : Int
     , buffer : Array Int
+    , stdout : Array Char
+    , stdin : Array Char
+    , stack : Array ( Int, Int )
     }
 
 
-type BFTarget
-    = Pointer
-    | Byte
-
-
-type BFDirection
-    = Start
-    | End
-
-
+{-| Debug format - eventually add runlengths for optimizing BF scripts
+-}
 type BFCommand
-    = Incr BFTarget
-    | Decr BFTarget
+    = IncrPtr
+    | DecrPtr
+    | IncrByte
+    | DecrByte
     | Write
     | Read
-    | Jump BFDirection
+    | StartBlock
+    | EndBlock
 
 
 decodeChar : Char -> Maybe BFCommand
 decodeChar char =
     case char of
         '>' ->
-            Just (Incr Pointer)
+            Just IncrPtr
 
         '<' ->
-            Just (Decr Pointer)
+            Just DecrPtr
 
         '+' ->
-            Just (Incr Byte)
+            Just IncrByte
 
         '-' ->
-            Just (Decr Byte)
+            Just DecrByte
 
         '.' ->
             Just Write
@@ -51,10 +49,10 @@ decodeChar char =
             Just Read
 
         '[' ->
-            Just (Jump End)
+            Just StartBlock
 
         ']' ->
-            Just (Jump Start)
+            Just EndBlock
 
         _ ->
             Nothing
@@ -72,10 +70,53 @@ goToNextCommand model =
     { model | position = model.position + 1 }
 
 
+processCurrentCommand : BFModel -> BFModel
 processCurrentCommand model =
-    case get model.position model.script of
+    let
+        currentCmd =
+            get model.position model.script
+                |> Maybe.withDefault 'x'
+                |> decodeChar
+    in
+    case currentCmd of
         Just cmd ->
-            Nothing
+            let
+                currentByte =
+                    get model.pointer model.buffer
+                        |> Maybe.withDefault 0
+            in
+            case cmd of
+                IncrPtr ->
+                    { model | pointer = model.pointer + 1 }
+
+                DecrPtr ->
+                    { model | pointer = model.pointer - 1 }
+
+                IncrByte ->
+                    { model | buffer = set model.pointer (currentByte + 1) model.buffer }
+
+                DecrByte ->
+                    { model | buffer = set model.pointer (currentByte - 1) model.buffer }
+
+                Write ->
+                    { model | stdout = push (Char.fromCode currentByte) model.stdout }
+
+                Read ->
+                    let
+                        readByte =
+                            'x'
+                                |> Char.toCode
+
+                        newStdin =
+                            slice 1 0 model.stdin
+                    in
+                    { model
+                        | stdin = newStdin
+                        , buffer = set model.pointer readByte model.buffer
+                    }
+
+                _ ->
+                    model
 
         Nothing ->
-            Nothing
+            model
